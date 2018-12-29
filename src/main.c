466b1897e3c9658e7b2f972b42d529d44e1080df
Andrew Bae <admin@pwner.kr>
2018-12-29 21:22:17 +0900

    ⚡  IMPROVE: refactoring the code and add FuncCreateButton function

--- src/main.c
+++ src/main.c
@@ -1,25 +1,20 @@
 #include <windows.h>
 
-#define WIDTH (500)
+#define WIDTH  (500)
 #define HEIGHT (800)
 
-LPCSTR WndClassName = TEXT("MyCalculator");
+LPCWSTR WndClassName = TEXT("DefaultUserInterface");
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
+int WINAPI FuncRegisterClass(HINSTANCE hInstance);
+HWND WINAPI FuncCreateButton(LPSTR WndButtonContext, UINT x, UINT y, UINT w, UINT h, HWND hWndParent);
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR CmdLine, int CmdShow)
-{
-	WNDCLASS wc = { NULL };
-	wc.lpfnWndProc = WndProc;
-	wc.hInstance = hInstance;
-	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.lpszMenuName = NULL;
-	wc.lpszClassName = WndClassName;
-	wc.hbrBackground = GetStockObject(WHITE_BRUSH);
-	RegisterClass(&wc);
-	
-	HWND hCalculator = CreateWindowEx(
+{	
+	FuncRegisterClass(hInstance);
+
+	HWND hwnd = CreateWindowEx(
 		NULL,
 		WndClassName,
 		TEXT("Calculator"),
@@ -28,7 +23,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR CmdLine, 
 		hInstance, NULL
 	);
 
-	ShowWindow(hCalculator, CmdShow);
+	ShowWindow(hwnd, CmdShow);
 
 	MSG msg;
 	while (GetMessage(&msg, NULL, NULL, NULL)) {
@@ -45,10 +40,10 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	COLORREF color;
 	switch (uMsg) {
 	case WM_PAINT:
-		color = RGB(255, 0, 0);
 		hdc = BeginPaint(hwnd, &ps);
 		TextOut(hdc, (WIDTH / 2), 0, TEXT("Calculator"), sizeof("Calculator"));
 		EndPaint(hwnd, &ps);
+		FuncCreateButton(TEXT("Shit"), 10, 10, 50, 20, hwnd);
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
@@ -56,5 +51,31 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	default:
 		return DefWindowProc(hwnd, uMsg, wParam, lParam);
 	}
+	UpdateWindow(hwnd);
 	return 0;
+}
+
+int WINAPI FuncRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASS wc = { NULL };
+	wc.lpfnWndProc = WndProc;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = WndClassName;
+	wc.hbrBackground = GetStockObject(WHITE_BRUSH);
+	return RegisterClass(&wc);
+}
+
+HWND WINAPI FuncCreateButton(LPSTR WndButtonContext, UINT x, UINT y, UINT w, UINT h, HWND hWndParent)
+{
+	HWND hWndButton = CreateWindow(
+		TEXT("BUTTON"),
+		WndButtonContext,
+		WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 
+		x, y, w, h,
+		hWndParent,
+		NULL, NULL, NULL);
+	return hWndButton;
 }
