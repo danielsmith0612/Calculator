2ebc32d99430b20176c7b7695e3d5252b7c0e3d1
Andrew Bae <admin@pwner.kr>
2019-01-04 18:44:34 +0900

    📖  DOC: modify IDC and add IDC for more expressions(add, sub, mul, div, dot, result)

--- header/main.h
+++ header/main.h
@@ -2,9 +2,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#define WIDTH  (500)
-#define HEIGHT (700)
+#define WIDTH  (201)
+#define HEIGHT (285)
 
+#define IDC_BUTTON_DOWN_0 (0)
 #define IDC_BUTTON_DOWN_1 (1)
 #define IDC_BUTTON_DOWN_2 (2)
 #define IDC_BUTTON_DOWN_3 (3)
@@ -14,6 +15,13 @@
 #define IDC_BUTTON_DOWN_7 (7)
 #define IDC_BUTTON_DOWN_8 (8)
 #define IDC_BUTTON_DOWN_9 (9)
+
+#define IDC_BUTTON_DOWN_ADD (10)
+#define IDC_BUTTON_DOWN_SUB (11)
+#define IDC_BUTTON_DOWN_MUL (12)
+#define IDC_BUTTON_DOWN_DIV (13)
+#define IDC_BUTTON_DOWN_DOT (14)
+#define IDC_BUTTON_DOWN_RESULT (15)
 
 int WINAPI FuncRegisterClass(HINSTANCE hInstance);
 HWND WINAPI FuncCreateButton(LPSTR string, UINT x, UINT y, UINT w, UINT h, HWND hParent);
--- src/main.c
+++ src/main.c
@@ -36,54 +36,52 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			hwnd, IDC_BUTTON_DOWN_7, NULL, NULL);
 		CreateWindow(TEXT("BUTTON"), TEXT("8"), WS_TABSTOP | WS_VISIBLE | WS_CHILD,
 			50, 65, ButtonSize, ButtonSize,
-			hwnd, IDC_BUTTON_DOWN_7, NULL, NULL);
+			hwnd, IDC_BUTTON_DOWN_8, NULL, NULL);
 		CreateWindow(TEXT("BUTTON"), TEXT("9"), WS_TABSTOP | WS_VISIBLE | WS_CHILD,
 			95, 65, ButtonSize, ButtonSize,
-			hwnd, IDC_BUTTON_DOWN_7, NULL, NULL);
+			hwnd, IDC_BUTTON_DOWN_9, NULL, NULL);
 		CreateWindow(TEXT("BUTTON"), TEXT("+"), WS_TABSTOP | WS_VISIBLE | WS_CHILD,
 			140, 65, ButtonSize, ButtonSize,
-			hwnd, IDC_BUTTON_DOWN_7, NULL, NULL);
+			hwnd, IDC_BUTTON_DOWN_ADD, NULL, NULL);
 
 		CreateWindow(TEXT("BUTTON"), TEXT("4"), WS_TABSTOP | WS_VISIBLE | WS_CHILD,
 			5, 110, ButtonSize, ButtonSize,
-			hwnd, IDC_BUTTON_DOWN_7, NULL, NULL);
+			hwnd, IDC_BUTTON_DOWN_4, NULL, NULL);
 		CreateWindow(TEXT("BUTTON"), TEXT("5"), WS_TABSTOP | WS_VISIBLE | WS_CHILD,
 			50, 110, ButtonSize, ButtonSize,
-			hwnd, IDC_BUTTON_DOWN_7, NULL, NULL);
+			hwnd, IDC_BUTTON_DOWN_5, NULL, NULL);
 		CreateWindow(TEXT("BUTTON"), TEXT("6"), WS_TABSTOP | WS_VISIBLE | WS_CHILD,
 			95, 110, ButtonSize, ButtonSize,
-			hwnd, IDC_BUTTON_DOWN_7, NULL, NULL);
+			hwnd, IDC_BUTTON_DOWN_6, NULL, NULL);
 		CreateWindow(TEXT("BUTTON"), TEXT("-"), WS_TABSTOP | WS_VISIBLE | WS_CHILD,
 			140, 110, ButtonSize, ButtonSize,
-			hwnd, IDC_BUTTON_DOWN_7, NULL, NULL);
+			hwnd, IDC_BUTTON_DOWN_SUB, NULL, NULL);
 
 		CreateWindow(TEXT("BUTTON"), TEXT("1"), WS_TABSTOP | WS_VISIBLE | WS_CHILD,
 			5, 155, ButtonSize, ButtonSize,
-			hwnd, IDC_BUTTON_DOWN_7, NULL, NULL);
+			hwnd, IDC_BUTTON_DOWN_1, NULL, NULL);
 		CreateWindow(TEXT("BUTTON"), TEXT("2"), WS_TABSTOP | WS_VISIBLE | WS_CHILD,
 			50, 155, ButtonSize, ButtonSize,
-			hwnd, IDC_BUTTON_DOWN_7, NULL, NULL);
+			hwnd, IDC_BUTTON_DOWN_2, NULL, NULL);
 		CreateWindow(TEXT("BUTTON"), TEXT("3"), WS_TABSTOP | WS_VISIBLE | WS_CHILD,
 			95, 155, ButtonSize, ButtonSize,
-			hwnd, IDC_BUTTON_DOWN_7, NULL, NULL);
+			hwnd, IDC_BUTTON_DOWN_3, NULL, NULL);
 		CreateWindow(TEXT("BUTTON"), TEXT("X"), WS_TABSTOP | WS_VISIBLE | WS_CHILD,
 			140, 155, ButtonSize, ButtonSize,
-			hwnd, IDC_BUTTON_DOWN_7, NULL, NULL);
+			hwnd, IDC_BUTTON_DOWN_MUL, NULL, NULL);
 
 		CreateWindow(TEXT("BUTTON"), TEXT("0"), WS_TABSTOP | WS_VISIBLE | WS_CHILD,
 			5, 200, ButtonSize, ButtonSize,
-			hwnd, IDC_BUTTON_DOWN_7, NULL, NULL);
+			hwnd, IDC_BUTTON_DOWN_0, NULL, NULL);
 		CreateWindow(TEXT("BUTTON"), TEXT("."), WS_TABSTOP | WS_VISIBLE | WS_CHILD,
 			50, 200, ButtonSize, ButtonSize,
-			hwnd, IDC_BUTTON_DOWN_7, NULL, NULL);
+			hwnd, IDC_BUTTON_DOWN_DOT, NULL, NULL);
 		CreateWindow(TEXT("BUTTON"), TEXT("="), WS_TABSTOP | WS_VISIBLE | WS_CHILD,
 			95, 200, ButtonSize, ButtonSize,
-			hwnd, IDC_BUTTON_DOWN_7, NULL, NULL);
+			hwnd, IDC_BUTTON_DOWN_RESULT, NULL, NULL);
 		CreateWindow(TEXT("BUTTON"), TEXT("/"), WS_TABSTOP | WS_VISIBLE | WS_CHILD,
 			140, 200, ButtonSize, ButtonSize,
-			hwnd, IDC_BUTTON_DOWN_7, NULL, NULL);
-
-
+			hwnd, IDC_BUTTON_DOWN_DIV, NULL, NULL);
 		break;
 	}
 	case WM_COMMAND: {
